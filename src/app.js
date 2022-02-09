df08ea4be388d2895a8b8acd8732787c05dad2d6
Lucas <novo.contato.lucas@gmail.com>
2022-02-08 21:23:04 -0300

    update 0.4

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ function RemoveSidebar() {
 
     const sidebar = document.querySelector('nav')
     const blackout = document.querySelector('.blackout')
+    blackout.classList.add('removegradient')
 
     sidebar.classList.remove('anim-show')
     sidebar.classList.add('anim-hidden')
@@ -33,6 +34,8 @@ function RemoveSidebar() {
     setTimeout(() => {
         sidebar.classList.add('hidden')
         sidebar.classList.remove('anim-hidden')
+
+        blackout.classList.remove('removegradient')
         blackout.classList.add('hidden')
     }, 2000);
 }
@@ -43,6 +46,38 @@ function ShowSidebar() {
     const blackout = document.querySelector('.blackout')
 
     blackout.classList.remove('hidden')
+
     sidebar.classList.add('anim-show')
     sidebar.classList.remove('hidden')
+}
+
+
+// desmarcar opções 
+// selecionar somente um usuário
+
+function SelectUser(user) {
+
+    user = user.querySelector('.user-selected')
+    user.classList.toggle('hidden')
+    // console.log(user)
+}
+
+document.addEventListener('keypress', GetUserMessage)
+
+function GetUserMessage(userkey) {
+
+    if (userkey.key === 'Enter' || userkey === 'button') {
+
+        let text = document.querySelector('#usertext').value
+        // console.log(text)
+        SendMessage(text)
+        document.querySelector('#submit').click();
+        document.querySelector('#usertext').value = ''
+    }
+}
+
+function SendMessage(text) {
+
+    if(text.length === 0) return
+    alert(text)
 }
