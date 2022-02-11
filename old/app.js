e687c14a7af6b4f7d53de67b80ff03d76b3ba9f3
i-Lucas <novo.contato.lucas@gmail.com>
2022-02-11 01:53:03 -0300

    update VERSION BETA 0.2.4

---
+++ old/app.js
@@ -0,0 +1,299 @@
+// Entrar()
+
+
+
+let UserName = {}
+
+// GetUserName()
+
+function GetUserName() {
+
+    let user = document.querySelector('.user-name').value
+    user = user.toString()
+    UserName = {
+        name: user
+    }
+    SendServerUserName(UserName)
+}
+
+function Entrar() {
+
+    const homescreen = document.querySelector('.home-screen')
+    homescreen.classList.add('hidden')
+
+    const main = document.querySelector('main')
+    main.classList.remove('hidden')
+
+    const sidebar = document.querySelector('nav')
+    sidebar.classList.add('hidden')
+}
+
+function RemoveSidebar() {
+
+    const sidebar = document.querySelector('nav')
+    const blackout = document.querySelector('.blackout')
+    blackout.classList.add('removegradient')
+
+    sidebar.classList.remove('anim-show')
+    sidebar.classList.add('anim-hidden')
+
+    setTimeout(() => {
+        sidebar.classList.add('hidden')
+        sidebar.classList.remove('anim-hidden')
+
+        blackout.classList.remove('removegradient')
+        blackout.classList.add('hidden')
+    }, 2000);
+}
+
+function ShowSidebar() {
+
+    const sidebar = document.querySelector('nav')
+    const blackout = document.querySelector('.blackout')
+
+    blackout.classList.remove('hidden')
+
+    sidebar.classList.add('anim-show')
+    sidebar.classList.remove('hidden')
+}
+
+let optionChecked = false
+let thisOptionChecked = 'none'
+let typeSelected = 'message'
+
+let userChecked = false
+let thisUserChecked = 'none'
+let userSelected = 'Todos'
+
+function SelectUser(user) {
+
+    user = user.querySelector('.user-selected')
+
+    if (userChecked === false) {
+        user.classList.remove('hidden')
+        thisUserChecked = user
+        userChecked = true
+    }
+
+    if (userChecked === true) {
+        thisUserChecked.classList.add('hidden')
+        user.classList.remove('hidden')
+        thisUserChecked = user
+    }
+
+    user = user.parentNode;
+    user = user.querySelector('p')
+    user = user.innerHTML
+
+    userSelected = user
+}
+
+function SelectOption(option, type) {
+
+    option = option.querySelector('.user-selected')
+
+    if (optionChecked === false) {
+
+        if (type === 'message') {
+            option.classList.remove('hidden')
+            thisOptionChecked = option
+            optionChecked = true
+
+            userSelected = 'Todos'
+            typeSelected = 'message'
+        }
+
+        if (type === 'private_message') {
+            option.classList.remove('hidden')
+            thisOptionChecked = option
+            optionChecked = true
+
+            typeSelected = 'private_message'
+        }
+    }
+
+    if (optionChecked === true) {
+
+        thisOptionChecked.classList.add('hidden')
+        option.classList.remove('hidden')
+        thisOptionChecked = option
+
+        typeSelected = type
+    }
+
+    console.log('tipo selecionado ' + typeSelected)
+}
+
+document.addEventListener('keypress', GetUserMessage)
+
+
+function GetTime() {
+
+    let data = new Date();
+    let hr = data.getHours();
+    let min = data.getMinutes();
+    let seg = data.getSeconds();
+    return `${hr}:${min}:${seg}`
+}
+
+function GetUserMessage(userkey) {
+
+    if (userkey.key === 'Enter' || userkey === 'button') {
+
+        let text = document.querySelector('#usertext').value
+        document.querySelector('#submit').click();
+        document.querySelector('#usertext').value = ''
+
+        console.log(typeSelected, text, userSelected)
+        SendMessage(UserName, typeSelected, text, userSelected, 0)
+
+        const msgObj = {
+
+            from: `${UserName}`,
+            to: `${userSelected}`,
+            text: `${text}`,
+            type: `${typeSelected}`
+        }
+
+
+        axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', msgObj)
+
+
+    }
+}
+
+
+function SendMessage(user, type, text, receive, time) {
+
+    if (text.length === 0) return
+
+    const page = document.querySelector('.page-content')
+
+    if (type === 'private_message') {
+
+        page.innerHTML += `
+
+        <div class="message ${type}">
+            <p><span class = 'span-time'>
+            (${time})</span> 
+            <span class = 'span-user'> ${user}</span>
+            reservadamente para<span class = 'span-user'>${receive}</span>:
+            <span class = 'span-message'>
+            ${text}</p></span>
+        </div>
+    `
+        return
+    }
+
+    if (type === 'message') {
+
+        page.innerHTML += `
+
+            <div class="message">
+                <p><span class = 'span-time'>
+                (${time})</span> 
+                <span class = 'span-user'> ${user}</span> para<span class = 'span-user'>${receive}</span>:
+                <span class = 'span-message'>
+                ${text}</p></span>
+            </div>
+        `
+        return
+    }
+
+    if (type === 'status') {
+
+
+        page.innerHTML += `
+
+        <div class="message status">
+            <p><span class = 'span-time'>
+            (${time})</span> 
+            <span class = 'span-user'> ${user}</span>
+            <span class = 'span-message'>
+            entra na sala ...</p></span>
+        </div>
+    `
+    }
+}
+
+function DeleteMessages() {
+
+    const page = document.querySelector('.page-content')
+    while (page.firstChild) {
+        page.removeChild(page.firstChild)
+    }
+}
+
+// UserConnected(GetTime(), 'Lucas')
+
+// conectar com o servidor
+// obter as mensagens 
+// enviar as mensagens 
+
+function SendServerUserName(userObj) {
+
+    const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', userObj)
+    promise.then(PromiseReceived); // deu tudo certo
+    promise.catch(ErrorReceived) // deu erro
+}
+
+let interval_CheckOnlineUser = undefined
+let interval_LoadMessages = undefined
+
+function PromiseReceived(promise) {
+
+    console.log(promise)
+    interval_CheckOnlineUser = setInterval(CheckOnlineUser(UserName), 5000)
+    interval_LoadMessages = setInterval(LoadMessagesPromise, 5000)
+    Entrar()
+        // LoadMessagesPromise()
+}
+
+function ErrorReceived(error) {
+
+    console.log('error code: ' + error.response.status)
+    console.log('error msg: ' + error.response.data)
+
+    if (error.response.status === 400) {
+        return alert('Já existe um usuário online com esse nickname. Por favor escolha outro')
+    }
+}
+
+// load messages
+function LoadMessagesPromise() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
+    promise.then(LoadMessages)
+    promise.catch(LoadMessagesError)
+}
+
+function LoadMessages(promise) {
+
+    console.log(promise.data)
+    let lista = promise.data
+
+    DeleteMessages()
+
+    for (let i = 0; i < lista.length; i++) {
+
+        // console.log(lista[i].from, lista[i].type, lista[i].text, lista[i].to, lista[i].time)
+        SendMessage(lista[i].from, lista[i].type, lista[i].text, lista[i].to, lista[i].time)
+    }
+
+}
+
+function LoadMessagesError(error) {
+    console.log('error code: ' + error.response.status)
+    console.log('error msg: ' + error.response.data)
+    return
+}
+
+// check if the user is online every 5 seconds
+function CheckOnlineUser(userObj) {
+
+    const promise = axios.post(' https://mock-api.driven.com.br/api/v4/uol/status', userObj)
+    promise.then(CheckOnlineUserPromise)
+    promise.catch(CheckOnlineUserError)
+}
+
+const CheckOnlineUserPromise = (promise) => console.log('user online')
+const CheckOnlineUserError = (error) => console.log('user offline')
--- src/app.js
+++ src/app.js
@@ -1,39 +1,176 @@
-// Entrar()
+let UserName = ''
+let UserOnline = false
 
-
-
-let UserName = {}
-
-// GetUserName()
+let userSelected = 'Todos'
+let typeSelected = 'message'
 
 function GetUserName() {
 
-    let user = document.querySelector('.user-name').value
-    user = user.toString()
-    UserName = {
-        name: user
-    }
-    SendServerUserName(UserName)
+    UserName = document.querySelector('.user-name').value
+    if (UserName.length >= 5) {
+        let promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', { name: UserName })
+        promise.then(PromiseNameReceived)
+        promise.catch(PromiseNameError)
+    } else
+        return alert('choose a name with at least 5 characters')
 }
 
-function Entrar() {
+function PromiseNameError(error) {
+    console.log(`error code: ${error.response.status} msg: ${error.response.data}`)
+    if (error.response.status === 400) {
+        return alert('There is already a user online with that username. Try again')
+    }
+}
 
-    const homescreen = document.querySelector('.home-screen')
+let IntervalSendStatusOnline
+let IntervalGetServerMessages
+
+function PromiseNameReceived(response) {
+    Login()
+    IntervalSendStatusOnline = setInterval(SendStatusOnline, 5000)
+    IntervalGetServerMessages = setInterval(GetServerMessagesPromise, 5000)
+}
+
+function SendStatusOnline() {
+    let promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', { name: UserName })
+    promise.then(ConfirmUserOnline)
+    promise.catch(DisconnectedUser)
+}
+
+function ConfirmUserOnline(promise) {
+
+    UserOnline = true
+    GetUsersOnline()
+}
+
+function DisconnectedUser(error) {
+
+    UserOnline = false
+    console.log('unable to check your status at this time')
+    console.log(`error code: ${error.response.status} msg: ${error.response.data}`)
+    CheckDisconectedUser()
+}
+
+function GetServerMessagesPromise() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
+    promise.then(RenderMessages)
+}
+
+function RenderMessages(promise) {
+    DeleteMessages()
+    let list = promise.data
+    for (let i = 0; i < list.length; i++) {
+        BuildMessages(list[i].from, list[i].to, list[i].text, list[i].type, list[i].time)
+    }
+}
+
+function DeleteMessages() {
+    let page = document.querySelector('.page-content')
+    if (page.firstChild !== null || page.firstChild !== undefined) {
+        while (page.firstChild) {
+            page.removeChild(page.firstChild)
+        }
+    }
+}
+
+function BuildMessages(from, to, text, type, time) {
+
+    const container = document.querySelector('.page-content')
+
+    if (type === 'status') {
+        container.innerHTML += `
+        <div class="message status">
+            <p><span class = 'span-time'>
+            (${time})</span> 
+            <span class = 'span-user'> ${from}</span>
+            <span class = 'span-message'>
+            ${text} ...</p></span>
+        </div>`
+    }
+    if (type === 'message') {
+        container.innerHTML += `
+        <div class="message">
+            <p><span class = 'span-time'>
+            (${time})</span> 
+            <span class = 'span-user'> ${from}</span> para<span class = 'span-user'>${to}</span>:
+            <span class = 'span-message'>
+            ${text}</p></span>
+        </div>`
+    }
+    if (type === 'private_message') {
+        container.innerHTML += `
+        <div class="message private">
+            <p><span class = 'span-time'>
+            (${time})</span> 
+            <span class = 'span-user'> ${from}</span>
+            reservadamente para<span class = 'span-user'>${to}</span>:
+            <span class = 'span-message'>
+            ${text}</p></span>
+        </div>`
+    }
+}
+
+function SendMessageUserToServer(to, text, type) {
+
+    let msgdata = {
+        from: UserName,
+        to: to,
+        text: text,
+        type: type
+    }
+
+    let time = GetUserTime()
+    BuildMessages(UserName, to, text, type, time)
+
+    let userpost = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', msgdata)
+    userpost.then(SendMessageUserToServerPromise)
+    userpost.catch(SendMessageUserToServerError)
+}
+
+function SendMessageUserToServerPromise(promise) {
+    // console.log('Message sent successfully')
+}
+
+function SendMessageUserToServerError(error) {
+    alert('It was not possible to send your message')
+    console.log(`error code: ${error.response.status} msg: ${error.response.data}`)
+}
+
+function CheckDisconectedUser() {
+    if (!UserOnline) {
+        Exit()
+    }
+}
+
+// login
+const homescreen = document.querySelector('.home-screen')
+const main = document.querySelector('main')
+
+// sidebar
+const blackout = document.querySelector('.blackout')
+const sidebar = document.querySelector('nav')
+
+function Exit() {
+    console.log('You have been disconnected')
+    homescreen.classList.remove('hidden')
+    main.classList.add('hidden')
+    sidebar.classList.remove('hidden')
+
+    clearInterval(IntervalSendStatusOnline)
+    clearInterval(IntervalGetServerMessages)
+}
+
+function Login() {
+    console.log('welcome to the jungle')
     homescreen.classList.add('hidden')
-
-    const main = document.querySelector('main')
     main.classList.remove('hidden')
-
-    const sidebar = document.querySelector('nav')
     sidebar.classList.add('hidden')
 }
 
+// sidebar 
 function RemoveSidebar() {
 
-    const sidebar = document.querySelector('nav')
-    const blackout = document.querySelector('.blackout')
     blackout.classList.add('removegradient')
-
     sidebar.classList.remove('anim-show')
     sidebar.classList.add('anim-hidden')
 
@@ -47,34 +184,85 @@ function RemoveSidebar() {
 }
 
 function ShowSidebar() {
-
-    const sidebar = document.querySelector('nav')
-    const blackout = document.querySelector('.blackout')
-
     blackout.classList.remove('hidden')
-
     sidebar.classList.add('anim-show')
     sidebar.classList.remove('hidden')
 }
 
+// send messages using enter
+document.addEventListener('keypress', GetUserMessage)
+
+function GetUserMessage(userkey) {
+
+    if (userkey.key === 'Enter' || userkey === 'button') {
+
+        let text = document.querySelector('#usertext').value
+        document.querySelector('#submit').click();
+        document.querySelector('#usertext').value = ''
+        SendMessageUserToServer(userSelected, text, typeSelected)
+    }
+}
+
+function GetUserTime() {
+    let data = new Date();
+    let hr = data.getHours();
+    let min = data.getMinutes();
+    let seg = data.getSeconds();
+    return `${hr}:${min}:${seg}`
+}
+
+// fill the sidebar with users who are online
+function GetUsersOnline() {
+
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/participants')
+    promise.then(GetUsersOnlinePromise)
+    promise.catch(GetUsersOnlineError)
+}
+
+function GetUsersOnlinePromise(promise) {
+
+    let UsersOnlineNames = []
+    let list = promise.data
+
+    for (let i = 0; i < list.length; i++) {
+        UsersOnlineNames.push(list[i].name)
+        FillSidebar(UsersOnlineNames)
+    }
+}
+
+function GetUsersOnlineError(error) {
+    console.log('could not get list of online users')
+    console.log(`error code: ${error.response.status} msg: ${error.response.data}`)
+}
+
+function FillSidebar(users) {
+
+    const sidebar = document.querySelector('nav .container')
+    sidebar.innerHTML = ''
+    for (let i = 0; i < users.length; i++) {
+        sidebar.innerHTML += `
+        <div class='display-flex box' onclick='SelectUser(this)'>
+            <ion-icon name="person-circle"></ion-icon>
+            <p class="spacing">${users[i]}</p>
+            <ion-icon name="checkmark-outline" class='user-selected hidden'></ion-icon>
+        </div>`
+    }
+}
+
 let optionChecked = false
 let thisOptionChecked = 'none'
-let typeSelected = 'message'
 
 let userChecked = false
 let thisUserChecked = 'none'
-let userSelected = 'Todos'
 
 function SelectUser(user) {
 
     user = user.querySelector('.user-selected')
-
     if (userChecked === false) {
         user.classList.remove('hidden')
         thisUserChecked = user
         userChecked = true
     }
-
     if (userChecked === true) {
         thisUserChecked.classList.add('hidden')
         user.classList.remove('hidden')
@@ -91,209 +279,42 @@ function SelectUser(user) {
 function SelectOption(option, type) {
 
     option = option.querySelector('.user-selected')
-
     if (optionChecked === false) {
 
         if (type === 'message') {
             option.classList.remove('hidden')
             thisOptionChecked = option
             optionChecked = true
-
             userSelected = 'Todos'
             typeSelected = 'message'
+            return
         }
-
         if (type === 'private_message') {
             option.classList.remove('hidden')
             thisOptionChecked = option
             optionChecked = true
-
             typeSelected = 'private_message'
+            return
         }
-    }
-
-    if (optionChecked === true) {
+    } else if (optionChecked === true) {
 
         thisOptionChecked.classList.add('hidden')
         option.classList.remove('hidden')
         thisOptionChecked = option
 
-        typeSelected = type
-    }
-
-    console.log('tipo selecionado ' + typeSelected)
-}
-
-document.addEventListener('keypress', GetUserMessage)
-
-
-function GetTime() {
-
-    let data = new Date();
-    let hr = data.getHours();
-    let min = data.getMinutes();
-    let seg = data.getSeconds();
-    return `${hr}:${min}:${seg}`
-}
-
-function GetUserMessage(userkey) {
-
-    if (userkey.key === 'Enter' || userkey === 'button') {
-
-        let text = document.querySelector('#usertext').value
-        document.querySelector('#submit').click();
-        document.querySelector('#usertext').value = ''
-
-        console.log(typeSelected, text, userSelected)
-        SendMessage(UserName, typeSelected, text, userSelected, 0)
-
-        const msgObj = {
-
-            from: `${UserName}`,
-            to: `${userSelected}`,
-            text: `${text}`,
-            type: `${typeSelected}`
+        if (type === 'message') {
+            userSelected = 'Todos'
         }
 
-
-        axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', msgObj)
-
-
-    }
-}
-
-
-function SendMessage(user, type, text, receive, time) {
-
-    if (text.length === 0) return
-
-    const page = document.querySelector('.page-content')
-
-    if (type === 'private_message') {
-
-        page.innerHTML += `
-
-        <div class="message ${type}">
-            <p><span class = 'span-time'>
-            (${time})</span> 
-            <span class = 'span-user'> ${user}</span>
-            reservadamente para<span class = 'span-user'>${receive}</span>:
-            <span class = 'span-message'>
-            ${text}</p></span>
-        </div>
-    `
+        typeSelected = type
         return
     }
-
-    if (type === 'message') {
-
-        page.innerHTML += `
-
-            <div class="message">
-                <p><span class = 'span-time'>
-                (${time})</span> 
-                <span class = 'span-user'> ${user}</span> para<span class = 'span-user'>${receive}</span>:
-                <span class = 'span-message'>
-                ${text}</p></span>
-            </div>
-        `
-        return
-    }
-
-    if (type === 'status') {
-
-
-        page.innerHTML += `
-
-        <div class="message status">
-            <p><span class = 'span-time'>
-            (${time})</span> 
-            <span class = 'span-user'> ${user}</span>
-            <span class = 'span-message'>
-            entra na sala ...</p></span>
-        </div>
-    `
-    }
 }
 
-function DeleteMessages() {
+// resta fazer:
 
-    const page = document.querySelector('.page-content')
-    while(page.firstChild) {
-        page.removeChild(page.firstChild)
-    }
-}
-
-// UserConnected(GetTime(), 'Lucas')
-
-// conectar com o servidor
-// obter as mensagens 
-// enviar as mensagens 
-
-function SendServerUserName(userObj) {
-
-    const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', userObj)
-    promise.then(PromiseReceived); // deu tudo certo
-    promise.catch(ErrorReceived) // deu erro
-}
-
-let interval_CheckOnlineUser = undefined
-let interval_LoadMessages = undefined
-
-function PromiseReceived(promise) {
-
-    console.log(promise)
-    interval_CheckOnlineUser = setInterval(CheckOnlineUser(UserName), 5000)
-    interval_LoadMessages = setInterval(LoadMessagesPromise, 5000)
-    Entrar()
-    // LoadMessagesPromise()
-}
-
-function ErrorReceived(error) {
-
-    console.log('error code: ' + error.response.status)
-    console.log('error msg: ' + error.response.data)
-
-    if (error.response.status === 400) {
-        return alert('Já existe um usuário online com esse nickname. Por favor escolha outro')
-    }
-}
-
-// load messages
-function LoadMessagesPromise() {
-    const promise = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
-    promise.then(LoadMessages)
-    promise.catch(LoadMessagesError)
-}
-
-function LoadMessages(promise) {
-
-    console.log(promise.data)
-    let lista = promise.data
-
-    DeleteMessages()
-
-    for (let i = 0; i < lista.length; i++) {
-
-        // console.log(lista[i].from, lista[i].type, lista[i].text, lista[i].to, lista[i].time)
-        SendMessage(lista[i].from, lista[i].type, lista[i].text, lista[i].to, lista[i].time)
-    }
-
-}
-
-function LoadMessagesError(error) {
-    console.log('error code: ' + error.response.status)
-    console.log('error msg: ' + error.response.data)
-    return
-}
-
-// check if the user is online every 5 seconds
-function CheckOnlineUser(userObj) {
-
-    const promise = axios.post(' https://mock-api.driven.com.br/api/v4/uol/status', userObj)
-    promise.then(CheckOnlineUserPromise)
-    promise.catch(CheckOnlineUserError)
-}
-
-const CheckOnlineUserPromise = (promise) => console.log('user online')
-const CheckOnlineUserError = (error) => console.log('user offline')
+// refatorar as funções SelectUser e SelectOption
+// ajustar Css do sidebar (nome dos usuarios)
+// ajustar largura máxima das caixas de mensagem
+// ajustar quebra de linha dos textos e espaçamento
+// verificar pq o blackout (animação de background sidebar) não está funcionando
